41ab58e644101a33c3943763a84b2f6cc308fff8
Kenny <91564832+kenny2408@users.noreply.github.com>
2022-08-08 13:46:55 -0400

    main

--- installer.sh
+++ installer.sh
@@ -7,9 +7,12 @@ echo 'Hangman game by Kenny Escalante be installed in the folder "$path"/hangman
 echo 'To start installing press enter'
 echo 'To cancel press ctrl + z and then go to the folder you want to install'
 echo 'version 1.0'
+# shellcheck disable=SC2162
+# shellcheck disable=SC2034
 read x
 
 mkdir "hangman_game"
+# shellcheck disable=SC2164
 cd hangman_game
 path-$(pwd)
 
